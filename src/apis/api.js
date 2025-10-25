0ba7656e8f01e295bc24c80caf9191e9dde74cc2
isubhamsr <shubhamroy12345@gmail.com>
2025-10-26 04:43:48 +0530

    add api gateway urls

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 
-// const GATEWAY_URL = "http://localhost:8080"; // Django Gateway
+const GATEWAY_URL = "http://127.0.0.1:8080/gateway";
 
-const log_processor_url = "http://127.0.0.1:8000";
-const analytices_service_url = "http://127.0.0.1:8081";
+// const log_processor_url = "http://127.0.0.1:8000";
+// const analytices_service_url = "http://127.0.0.1:8081";
 
 export const uploadLog = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
-  const res = await axios.post(`${log_processor_url}/upload/`, formData, {
+  const res = await axios.post(`${GATEWAY_URL}/upload-log/`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
   return res.data;
 };
 
 export const getDashboard = async (unique_key) => {
-  const res = await axios.post(`${analytices_service_url}/dashboard/`, {
+  const res = await axios.post(`${GATEWAY_URL}/process-analytics/`, {
     unique_key,
   });
   return res.data;
 };
 
 export const insertData = async (unique_key) => {
-  const res = await axios.post(`${analytices_service_url}/add/`, {
+  const res = await axios.post(`${GATEWAY_URL}/run-kafka/`, {
     unique_key,
   });
   return res.data;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,23 +3,23 @@ import { getDashboard } from "../apis/api";
 
 export default function Dashboard({ unique_name }) {
   const [message, setMessage] = useState("");
-  const [summary, setSummary] = useState({
-    summary: {
-      unique_key: "",
-      total: 0,
-      success: 0,
-      client_error: 0,
-      server_error: 0,
-      top_endpoint: "",
-      top_ip: "",
-      method_counts: {
-        PUT: 0,
-        GET: 0,
-        DELETE: 0,
-        POST: 0,
-      },
-    },
-  });
+  const [summary, setSummary] = useState(null);
+
+  //   summary: {
+  //       unique_key: "",
+  //       total: 0,
+  //       success: 0,
+  //       client_error: 0,
+  //       server_error: 0,
+  //       top_endpoint: "",
+  //       top_ip: "",
+  //       method_counts: {
+  //         PUT: 0,
+  //         GET: 0,
+  //         DELETE: 0,
+  //         POST: 0,
+  //       },
+  //     },
 
   useEffect(() => {
     fetchDashboardData();
@@ -28,7 +28,7 @@ export default function Dashboard({ unique_name }) {
   async function fetchDashboardData() {
     setTimeout(async () => {
       await fetchData();
-    }, 10000);
+    }, 20000);
   }
 
   async function fetchData() {
@@ -44,58 +44,60 @@ export default function Dashboard({ unique_name }) {
   }
   return (
     <>
-      <section class="text-gray-600 body-font">
-        <div class="container px-5 py-16 mx-auto">
-          <div class="flex flex-wrap -m-4 text-center">
-            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                <h2 class="title-font font-medium text-3xl text-gray-900">
-                  {summary.total || 0}
-                </h2>
-                <p class="leading-relaxed">Number of Rows</p>
-              </div>
-            </div>
-            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                <h2 class="title-font font-medium text-3xl text-gray-900">
-                  {summary.success || 0}
-                </h2>
-                <p class="leading-relaxed">Success</p>
-              </div>
-            </div>
-            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                <h2 class="title-font font-medium text-3xl text-gray-900">
-                  {summary.client_error || 0}
-                </h2>
-                <p class="leading-relaxed">Client Error</p>
-              </div>
-            </div>
-            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                <h2 class="title-font font-medium text-3xl text-gray-900">
-                  {summary.server_error || 0}
-                </h2>
-                <p class="leading-relaxed">Server Error</p>
-              </div>
-            </div>
-            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                <h2 class="title-font font-medium text-3xl text-gray-900">
-                  {summary.top_endpoint || 0}
-                </h2>
-                <p class="leading-relaxed">Top Endpoint</p>
-              </div>
-            </div>
-            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                <h2 class="title-font font-medium text-3xl text-gray-900">
-                  {summary.top_ip || 0}
-                </h2>
-                <p class="leading-relaxed">Top Ip</p>
-              </div>
-            </div>
-            {/* <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+      {summary !== null ? (
+        <>
+          <section class="text-gray-600 body-font">
+            <div class="container px-5 py-16 mx-auto">
+              <div class="flex flex-wrap -m-4 text-center">
+                <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                  <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                    <h2 class="title-font font-medium text-3xl text-gray-900">
+                      {summary.total || 0}
+                    </h2>
+                    <p class="leading-relaxed">Number of Rows</p>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                  <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                    <h2 class="title-font font-medium text-3xl text-gray-900">
+                      {summary.success || 0}
+                    </h2>
+                    <p class="leading-relaxed">Success</p>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                  <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                    <h2 class="title-font font-medium text-3xl text-gray-900">
+                      {summary.client_error || 0}
+                    </h2>
+                    <p class="leading-relaxed">Client Error</p>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                  <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                    <h2 class="title-font font-medium text-3xl text-gray-900">
+                      {summary.server_error || 0}
+                    </h2>
+                    <p class="leading-relaxed">Server Error</p>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                  <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                    <h2 class="title-font font-medium text-3xl text-gray-900">
+                      {summary.top_endpoint || 0}
+                    </h2>
+                    <p class="leading-relaxed">Top Endpoint</p>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                  <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                    <h2 class="title-font font-medium text-3xl text-gray-900">
+                      {summary.top_ip || 0}
+                    </h2>
+                    <p class="leading-relaxed">Top Ip</p>
+                  </div>
+                </div>
+                {/* <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
               <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
                 <p class="title-font font-medium text-gray-900">
                   POST - {summary.method_counts.POST || 0}
@@ -111,16 +113,30 @@ export default function Dashboard({ unique_name }) {
                 </p>
               </div>
             </div> */}
+              </div>
+            </div>
+          </section>
+
+          <button
+            onClick={fetchData}
+            class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            Refresh Dashboard
+          </button>
+        </>
+      ) : (
+        <div class="container px-5 py-16 mx-auto">
+          <div role="status" class="max-w-sm animate-pulse">
+            <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+            <span class="sr-only">Loading...</span>
           </div>
         </div>
-      </section>
-
-      <button
-        onClick={fetchData}
-        class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-      >
-        Refresh Dashboard
-      </button>
+      )}
     </>
   );
 }
